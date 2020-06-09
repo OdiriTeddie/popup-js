a4ea72dcee7fb4dbc1f06a936e11343e2382e351
odiri teddie adamu <odiriteddie.adamu@gmail.com>
2020-06-09 11:28:12 -0500

    popup js exercise

---
+++ popup.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup-wrapper');
+const close = document.querySelector('.popup-close')
+
+button.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+popup.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
